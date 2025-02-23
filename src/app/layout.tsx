99deac162467a78ef087a37bb0623ff018b4bfd0
Jander Gomez <jandergb.30gmail.com>
2025-02-23 01:33:06 +0100

    fix: modify Dockerfile

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import { ClerkProvider } from '@clerk/nextjs'
 import './globals.css'
-import Navbar from "@/components/customs/Navbar";
+// import Navbar from "@/components/customs/Navbar";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
